7f200c7fda5009aa7e362c1d0a865ae334525dd8
AlchemistX <hick4321@gmail.com>
2013-02-06 16:49:55 +0900

    Speed Up!!

--- 001.BOGGLE/BOGGLE.py
+++ 001.BOGGLE/BOGGLE.py
@@ -1,58 +1,47 @@
 #!/usr/bin/env python
-gnWidth = 7
-gnHeight = 7
-lstCoord = [ -gnWidth-1, -gnWidth-0, -gnWidth+1, -1, +1, gnWidth-1, gnWidth+0, gnWidth+1]
+lstCoord = [ -8, -7, -6, -1, +1, 6, 7, 8]
 
-def step(strBoard, nStart, strWord, nCur):
-    if nCur == len(strWord):
+def step(strBoard, nStart, strWord, nLength, nCur):
+    if nCur == nLength:
         return True
 
     for delta in lstCoord:
         idx = nStart + delta
         if strBoard[idx] == strWord[nCur]:
-            if step(strBoard, idx, strWord, nCur+1) == True:
+            if step(strBoard, idx, strWord, nLength, nCur+1) == True:
                 return True
 
     return False
 
-def doBoggleGame(strBoard, lstWords):
-    for strWord in lstWords:
-        fGO = True
-        for C in strWord:
-            if strBoard.find(C) < 0:
-                print "%s NO" % (strWord)
-                fGO = False
-                break
+def doBoggleGame(strBoard, strWord):
+    
+    if set(strWord) - set(strBoard):
+        print "%s NO" % (strWord)
+        return
 
-        if fGO :
-            idx = strBoard.find(strWord[0])
-            while (idx > 0):
-                if step(strBoard, idx, strWord, 1) == True:
-                    print "%s YES" % (strWord)
-                    break
-                idx = strBoard.find(strWord[0], idx+1)
+    idx = strBoard.find(strWord[0])
+    while (idx > 0):
+        if step(strBoard, idx, strWord, len(strWord), 1) == True:
+            print "%s YES" % (strWord)
+            return
+        idx = strBoard.find(strWord[0], idx+1)
 
-            if idx < 0 :
-                print "%s NO" % (strWord)
+    print "%s NO" % (strWord)
 
 def main():
-    strBoard = ""
+    strPadding = "0000000"
     nStageCount = raw_input()
-    for i in xrange(0, int(nStageCount)) :
+    for _ in xrange(0, int(nStageCount)) :
         strBoard =""
-        strBoard += '0' * gnWidth
+        strBoard += strPadding
         for n in xrange(0, 5) :
             strBoard += '0'
             strBoard += raw_input()
             strBoard += '0'
-        strBoard += '0' * gnWidth
+        strBoard += strPadding
 
-        lstWords = []
-        nWordCount = raw_input()
-        for n in xrange(0, int(nWordCount)) :
-            lstWords.append(raw_input())
-
-        doBoggleGame(strBoard, lstWords)
+        for _ in xrange(0, int(raw_input())) :
+            doBoggleGame(strBoard, raw_input())
 
 if __name__ == "__main__" :
     main()
